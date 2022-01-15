596ae676b508952ae437a35ce9b6973aea053a51
Kęstutis Keršis <kestu.kersis@gmail.com>
2022-01-15 17:38:26 +0200

    Add project files.

---
+++ RabbitMQSender/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            // 1. create connection
+            using var connection = factory.CreateConnection();
+
+            // 2. create channel
+            using var channel = connection.CreateModel();
+            // 3. connect to the queue
+            channel.QueueDeclare(queue: "products",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            int index = 1;
+            while (index <= 99999)
+            {
+                string message = $"{index}|Product{10000 + index}|Potato|3|{DateTime.UtcNow.ToLongDateString()}|1|Poland";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // push content into the queue
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "products",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+                index++;
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
